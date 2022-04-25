4d1785cf7c60f5a4344d6b530e870041b8bd45bf
Dmytro Khomenko <dmytro.khomenko.it@gmail.com>
2022-04-26 01:58:27 +0300

    Завантаження ДЗ
    Завдання 1 - бібліотека SimpleLightbox;
    Завдання 2 - відеоплеєр;
    Завдання 3 - форма зворотного зв'язку

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+    import throttle from 'lodash.throttle';
+    import Player from '@vimeo/player';
 
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe, {
+    id: 19231868,
+    width: 640
+    });
+
+    const onPlay = function(date) {
+        localStorage.setItem('videoplayer-current-time', date.seconds);
+        console.log(parseInt(localStorage.getItem('videoplayer-current-time')))
+    };
+
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
